cecbe112c8c82e40f5439cb64dee065198abc10f
Christophe Pires <iglesias.pires@gmail.com>
2024-07-29 15:56:51 +0200

    Etape 9 tags & Hosts ok. RAF : le css :(. Sinon all is good izipizi

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import '../styles/tag.scss';
 
 const Tag = ({ tag }) => {
-  return (
-    <span className="tag">{tag}</span>
-  );
+  return <span className="tag">{tag}</span>;
 };
 
 export default Tag;
--- src/pages/Property.js
+++ src/pages/Property.js
@@ -4,6 +4,8 @@ import properties from '../annonces.json';
 import Slideshow from '../components/Slideshow';
 import Collapse from '../components/Collapse';
 import Rating from '../components/Rating';
+import Tag from '../components/Tag';
+import Host from '../components/Host';
 
 const Property = () => {
   const { id } = useParams();
@@ -19,7 +21,13 @@ const Property = () => {
       <div className="property-details">
         <h1>{property.title}</h1>
         <p>{property.location}</p>
+        <div className="tags">
+          {property.tags.map(tag => (
+            <Tag key={tag} tag={tag} />
+          ))}
+        </div>
         <Rating rating={property.rating} />
+        <Host host={property.host} />
         <Collapse title="Description">
           <p>{property.description}</p>
         </Collapse>
